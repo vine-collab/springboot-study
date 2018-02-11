901fe560b5b3fa5ec717d8db9c4664e1b4f9fe99
hekai <hekai0118@gmail.com>
2018-02-11 18:29:18 +0800

    spring boot start

--- springboot-study/src/main/java/com/blueberry/springboot/App2.java
+++ springboot-study/src/main/java/com/blueberry/springboot/App2.java
@@ -1,7 +1,6 @@
 package com.blueberry.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,8 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
  * @create 2018-02-11-17:39
  */
 //@SpringBootApplication
-//@ComponentScan
-public class App {
+@ComponentScan
+public class App2 {
 
     @Bean
     public Runnable createRunnable() {
@@ -21,7 +20,11 @@ public class App {
 
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+        SpringApplication springApplication = new SpringApplication(App2.class);
+
+        ConfigurableApplicationContext context = springApplication.run(args);
+        
+//        ConfigurableApplicationContext context = SpringApplication.run(App2.class, args);
         context.getBean(Runnable.class).run();
     }
 
