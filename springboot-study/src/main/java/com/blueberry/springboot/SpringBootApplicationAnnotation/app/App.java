e8e8c3b622730764b00b1173121b1a32e7f9df09
hekai <hekai0118@gmail.com>
2018-02-16 19:08:21 +0800

    initial springboot web project

--- springboot-study/src/main/java/com/blueberry/springboot/SpringBootApplicationAnnotation/app/App.java
+++ springboot-study/src/main/java/com/blueberry/springboot/SpringBootApplicationAnnotation/app/App.java
@@ -10,6 +10,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 启动流程：
+ * 1、判断是不是web环境
+ * 2、加载所有classpath下META/INF/spring.factories ApplicationContextInitializer
+ * 3、加载所有classpath下META/INF/spring.factories ApplicationListener
+ * 4、推断main方法所在类
+ * 5、开始执行run方法
+ * 6、设置java.awt.headless系统变量
+ * 7、加载所有classpath下META/INF/spring.factories SpringApplicationRunListener
+ * 8、执行所有SpringApplicationRunListener的started方法
+ * 9、实例化ApplicationArguments对象
+ * 10、创建environment
+ * 11、配置environment，主要是把run方法的参数配置到environment
+ * 12、执行所有的SpringApplicationRunListener的environmentPrepared方法
+ * 13、如果不是web环境但是web的environment，转换environment为标准的environment
+ * 14、输出banner
+ *
  * @author hekai
  * @create 2018-02-14-19:20
  */
---
+++ springboot-web/src/main/java/App.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hekai
+ * @create 2018-02-16-17:52
+ */
+@SpringBootApplication
+public class App {
+
+}
