90b98baaf60bd25351b9c76f455c20bbcb6bf1c5
hekai <hekai0118@gmail.com>
2018-02-16 22:24:57 +0800

    web

---
+++ springboot-web/src/main/java/com/blueberry/webapi/App.java
@@ -0,0 +1,17 @@
+package com.blueberry.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author hekai
+ * @create 2018-02-16-17:52
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ springboot-web/src/main/java/com/blueberry/webapi/UserController.java
@@ -0,0 +1,18 @@
+package com.blueberry.webapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hekai
+ * @create 2018-02-16-19:14
+ */
+@RestController
+public class UserController {
+
+
+    @GetMapping("/ping")
+    public String hello(){
+        return "hello spring boot!";
+    }
+}
