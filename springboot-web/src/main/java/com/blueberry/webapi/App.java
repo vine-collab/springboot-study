75ebb93b7b42042352e0eadd91c742f0bc4d001f
hekai <hekai0118@gmail.com>
2018-02-20 14:19:07 +0800

    metrics counterservice&gaugeservice

--- springboot-web/src/main/java/com/blueberry/webapi/App.java
+++ springboot-web/src/main/java/com/blueberry/webapi/App.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.PropertySource;
  * 异常处理：
  * 除去springboot默认异常@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
  *
+ * metric查看：
+ * CounterService.increment统计调用
+ * GaugeService.submit统计某个值
  *
  * @author hekai
  * @create 2018-02-16-17:52
--- springboot-web/src/main/java/com/blueberry/webapi/UserController.java
+++ springboot-web/src/main/java/com/blueberry/webapi/UserController.java
@@ -2,7 +2,9 @@ package com.blueberry.webapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,6 +17,9 @@ public class UserController {
     @Autowired
     private CounterService counterService;
 
+    @Autowired
+    private GaugeService gaugeService;
+
     @GetMapping("/ping")
     public String hello(){
         System.out.println("hello spring boot!");
@@ -26,5 +31,11 @@ public class UserController {
     public String error(){
         int i = 1 / 0;
         return "hello spring boot!";
+    }
+
+    @GetMapping("/testGaugeService")
+    public String testGaugeService(@RequestParam(value = "name", required = false) Long value){
+        gaugeService.submit("testGaugeService.value", value);
+        return "testGuageService";
     }
 }
