79409164714e958bf5db2c8c776be2761fbbd9f3
hekai <hekai0118@gmail.com>
2018-02-20 11:34:55 +0800

    配置文件定制日志

--- springboot-study/src/main/java/com/blueberry/springboot/log/App.java
+++ springboot-study/src/main/java/com/blueberry/springboot/log/App.java
@@ -6,6 +6,18 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 启动参数：--debug
+ * springboot默认的日志级别是info
+ * 可以通过logging.level.*=debug来设置，*可以为包，或者是某个类
+ *
+ * 日志级别：TRACE,DEBUG,WARN,ERROR,FATAL,OFF
+ *
+ * 日志文件10M为切割
+ * logging.file指定日志文件
+ * logging.path指定日志文件位置，文件名默认spring.log
+ *
+ * 日志格式：
+ * logging.pattern.console配置控制台日志格式
+ * logging.file.console配置文件日志格式
  * @author hekai
  * @create 2018-02-20-10:24
  */
