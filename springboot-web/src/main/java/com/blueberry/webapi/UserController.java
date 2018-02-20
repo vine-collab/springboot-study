12c20682dd51e70d9bb1cf55dc5c8fc2cd34990d
hekai <hekai0118@gmail.com>
2018-02-20 13:56:32 +0800

    health metric

--- springboot-web/src/main/java/com/blueberry/webapi/UserController.java
+++ springboot-web/src/main/java/com/blueberry/webapi/UserController.java
@@ -1,5 +1,7 @@
 package com.blueberry.webapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    private CounterService counterService;
 
     @GetMapping("/ping")
     public String hello(){
         System.out.println("hello spring boot!");
+        counterService.increment("ping.count");
         return "hello spring boot!";
     }
 
